14e5ac221e7d3538eaba34f345fb6ad08cb4db1f
guerrerocampos <jrheezy.gc@gmail.com>
2021-02-16 19:53:14 -0800

    finished assessment

--- code-challenges.js
+++ code-challenges.js
@@ -73,22 +73,22 @@
 // --------------------4) Create a function that removes all the vowels from a string.
 // Use the test variables provided below. Expected output: "HyThrLrnStdnt" "LvJvScrpt"
 
-// var stringWithVowels1 = "HeyThereLearnStudent"
-// var stringWithVowels2 = "ILoveJavaScript"
+var stringWithVowels1 = "HeyThereLearnStudent"
+var stringWithVowels2 = "ILoveJavaScript"
 
-// const removedVowels = (array) => {
-// // // 	// Create variable to store the letters
-//     let letters = array.split(""); 
-//     let letters2 =letters.filter(value => {
-//         if (! ["a", "e", "i", "o", "u"].includes(value)) {
-//             return true
-//         } else {
-//             return false
-//         }
-//     })
-//     return letters2.join('')
-// }
-// console.log(removedVowels(stringWithVowels1))
+const removedVowels = (array) => {
+// // 	// Create variable to store the letters
+    let letters = array.split(""); 
+    let letters2 =letters.filter(value => {
+        if (! ["a", "e", "i", "o", "u"].includes(value)) {
+            return true
+        } else {
+            return false
+        }
+    })
+    return letters2.join('')
+}
+console.log(removedVowels(removedVowels(stringWithVowels2)))
 // //         // create an argument that check to see if statements are true
 //             // if true, then it will push whatever does not match the vowels provided
               
@@ -97,29 +97,29 @@
 // --------------------5) Copy/paste your code from #4. Refactor your code to include non-string edge cases. Inform your user that the variable passed into the vowel removal function is not a string.
 // Use the test variables provided below. Expected output: "true is not a string" "42 is not a string" "mCdngMstr"
 
-var refactorTester1 = true
-var refactorTester2 = 42
-var refactorTester3 = "IAmACodingMaster"
-// pasted the previous syntax from questions 4.
-// need function to include nonstreing edges
-const removedVowels = (array) => {
-        let letters = array.split(""); 
-        let letters2 =letters.filter(value => {
-            // need it to check for uppercase and lowercase letters
-            if (! ["a", "e", "i", "o", "u"].includes(value)) {
-                // need it to check nonstring edges
-                return true
-            } else {
-                return false
-            }
-        })
+// var refactorTester1 = true
+// var refactorTester2 = 42
+// var refactorTester3 = "IAmACodingMaster"
+// // pasted the previous syntax from questions 4.
+// // need function to include nonstreing edges
+// const removedVowels = (array) => {
+//         let letters = array.split(""); 
+//         let letters2 =letters.filter(value => {
+//             // need it to check for uppercase and lowercase letters
+//             if (! ["a", "e", "i", "o", "u"].includes(value)) {
+//                 // need it to check nonstring edges
+//                 return true
+//             } else {
+//                 return false
+//             }
+//         })
         
- if (typeof value === "number" || typeof value === "boolean" {
-    return `${value} is not a string`
-    return letters2.join('')
-}
-    }
-    console.log(removedVowels(refactorTester2))
+//  if (typeof value === "number" || typeof value === "boolean" {
+//     return `${value} is not a string`
+//     return letters2.join('')
+// }
+//     }
+//     console.log(removedVowels(refactorTester2))
     // can't get it to read the uppercase letters too. i know i have to put .toLowerCase
     // but i can't find anywhere to put it
 
@@ -143,18 +143,22 @@ const removedVowels = (array) => {
 // }
 
 // console.log(highestNumWord(utensils))
-// im stuck
+// im stuck!!!!!!!!
 
 // --------------------7) Create a function that takes in an array of strings and returns an array with the last letter removed from each string.
 // Expected output: ["Luig", "Peac", "Toa", "Yosh"] ["Ink", "Blink", "Pink", "Clyd"]
 
-var mario = ["Luigi", "Peach", "Toad", "Yoshi"]
-var pacman = ["Inky", "Blinky", "Pinky", "Clyde"]
-// create the function that tales an array
-// i need it to return an array with the last letters removed from each string
+// var mario = ["Luigi", "Peach", "Toad", "Yoshi"]
+// var pacman = ["Inky", "Blinky", "Pinky", "Clyde"]
+// // create the function that tales an array
+// // i need it to return an array with the last letters removed from each string
 
-
-// need to split the strings
+// const letterRemoved = (array) => {
+//     return array.map((array) => {
+//         return array.slice(0, -1)
+//     })
+// }
+// console.log(letterRemoved(mario))
 // need to identfy the last letter.... .push() maybe?
 // i need to remove it
 // i need a return with the last letter removed .....
